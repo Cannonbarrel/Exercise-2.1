0122dc1d7871018b809269147d0fa58d5dfc3db5
Cannonbarrel <Firenoob1001@gmail.com>
2024-06-02 22:18:40 +0000

    Program for Exercise 2.1

--- main.cs
+++ main.cs
@@ -2,6 +2,12 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
-}
+            
+            const double CentimetersInInch = 2.54;
+            double inches = 5; 
+
+            double centimeters = inches * CentimetersInInch;
+
+            Console.WriteLine("{0} inches is {1} centimeters.", inches, centimeters);
+        }
+    }
